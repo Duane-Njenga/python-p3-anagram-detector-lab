4b9124bca4a2fe2d806aeae1c7a897580129c536
Dwayne <dune.njenga@student.moringaschool.com>
2025-05-10 08:32:07 +0300

    Completed Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, init_word):
+        self.init_word = init_word
+
+    def match(self, word_list):
+        matched_list = []
+        for word in word_list: 
+
+            if(sorted(list(self.init_word)) == sorted(list(word))):  
+               matched_list.append(word) 
+        return matched_list
+            
+                         
+
